45ddd0d33b1fc649cbbe28fc7ecd23bfaa3ef228
Sanad M <esanad@gmail.com>
2023-09-02 12:57:19 +0300

    Normalized line endings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        # "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "DIRS": [os.path.join(BASE_DIR, "../frontend/build")],
 
         "APP_DIRS": True,
