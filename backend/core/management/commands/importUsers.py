0499e4008907d5e15a68a6cdc2cefdccf49f731b
Sanad - Work <esanad@gmail.com>
2024-05-13 13:30:54 +0300

    organizing, creating demos

--- backend/core/management/commands/importUsers.py
+++ backend/core/management/commands/importUsers.py
@@ -1,116 +1,190 @@
-import os
 import pandas as pd
 from django.core.management.base import BaseCommand
-from django.db.utils import IntegrityError
+from django.db import connection
 from apps.auths.models import User
-from django.core.files import File
-from django.core.exceptions import ValidationError  # <-- Add this import
-from utils.validators import today, civil_validator, phone_validator  
+from django.db.models import ImageField, CharField, ForeignKey, DateField
+from django.utils import timezone
+from datetime import datetime
+
+# Define a naive datetime object
+naive_datetime = datetime(2022, 1, 1, 12, 0, 0)
+
+# Convert naive datetime to timezone-aware datetime
+aware_datetime = timezone.make_aware(naive_datetime)
 
 
 class Command(BaseCommand):
-    help = "Imports users from an Excel file into the database"
+    help = "Imports or updates users from an Excel file into the database based on the specified schema"
 
     def handle(self, *args, **options):
-        file_path = "core/management/data/elections.xlsx"
-        df = pd.read_excel(file_path, sheet_name="users")
 
+        # Define the file path
+        file_path = "core/management/startup/q8tasweet.xlsx"
+        work_sheet = "users"
+        required_data = [
+            "id",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password",
+            "is_superuser",
+            "is_staff",
+            "is_active",
+            "description",
+            "twitter",
+            "instagram",
+            "deleted",
+            "token",
+            ##
+            ## Images
+            "image",
+            "background",
+            ##
+            ## Numbers
+            "civil",
+            "phone",
+            "gender",
+            # #
+            # # ForeignKeys
+            "created_by_id",
+            "updated_by_id",
+            "deleted_by_id",
+            # #
+            # # DateTime
+            "token_expiry",
+            "created_at",
+            "updated_at",
+            "deleted_at",
+            "date_of_birth",
+            "date_joined",
+            "last_login",
+        ]
+
+        # Read data from Excel file
+        try:
+            df = pd.read_excel(file_path, sheet_name=work_sheet)
+            df = df[required_data]
+
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Failed to read Excel file: {e}"))
+            return
+
+        # Check if all required columns are present
+        required_columns = required_data
+        if not all(column in df.columns for column in required_columns):
+            missing_columns = ", ".join(
+                column for column in required_columns if column not in df.columns
+            )
+            self.stdout.write(
+                self.style.ERROR(
+                    f"Missing required columns in the Excel file: {missing_columns}"
+                )
+            )
+            return
+
+        # Initialize counters
         created_count = 0
         updated_count = 0
-        ignored_count = 0
 
+        # Iterate over each row in the DataFrame
         for index, row in df.iterrows():
-            user_id = row["id"]
-            image_path = row["image"] if pd.notna(row["image"]) else None
-            background_path = row["background"] if pd.notna(row["background"]) else None
-
-            image_file = None
-            background_file = None
-            if image_path and os.path.exists(image_path):
-                image_file = File(open(image_path, "rb"))
-            if background_path and os.path.exists(background_path):
-                background_file = File(open(background_path, "rb"))
-
-
-            # Check if the gender is valid or set to default
-            gender_value = row.get('gender')
-            if pd.isna(gender_value) or gender_value == '':
-                gender_value = None  # Or use GenderOptions.UNDEFINED or any default you prefer
-
-
-            civil_value = str(int(row['civil'])) if pd.notna(row['civil']) else None
-            phone_value = str(int(row['phone'])) if pd.notna(row['phone']) else None
-            # Validate the civil number
             try:
-                civil_validator(civil_value)
-            except ValidationError as e:
-                self.stdout.write(self.style.WARNING(f"Invalid civil number for user {row['username']}: {e}"))
-                ignored_count += 1
-                continue  # Skip this user and go to the next row
+                # Extract the 'id' separately if it's always present
+                user_id = row["id"]
+                # Prepare defaults excluding 'id'
+                defaults = {col: row[col] for col in df.columns if col != "id"}
 
+                for field in User._meta.fields:
+                    # Check and handle ImageField dynamically
+                    if isinstance(field, ImageField) and field.name in defaults:
+                        field_value = defaults.get(field.name)
+                        if pd.isna(field_value) or field_value == "":
+                            defaults[field.name] = None  # Set field to None if empty
 
-            try:
-                user, created = User.objects.update_or_create(
-                    defaults={
-                        "username": row["username"],
-                        "email": row["email"],
-                        "password": row["password"],
-                        "is_superuser": row["is_superuser"],
-                        "first_name": row["first_name"],
-                        "last_name": row["last_name"],
-                        'description': row['description'],
-                        
-                        'twitter': row['twitter'],
-                        'instagram': row['instagram'],
-                        'is_staff': row['is_staff'],
-                        'is_active': row['is_active'],
-                        'token': row['token'],
-                        # 'token_expiry': row['token_expiry'],
-                        
-                        
-                        # image, Gender validation if nan, None
-                        "image": image_file,
-                        "background": background_file,
-                        'gender': gender_value,
-                        
-                        # Number Validation
-                        "civil": civil_value,  # Use the cleaned civil_value
-                        'phone': phone_value,
-                        
-                        
-                        # DateTime Validation
-                    },
-                    id=user_id,
+                    # Check and handle CharField with max_length
+                    if (
+                        isinstance(field, CharField)
+                        and field.max_length
+                        and field.name in defaults
+                    ):
+                        field_value = defaults.get(field.name)
+                        if field_value and len(str(field_value)) > field.max_length:
+                            defaults[field.name] = str(field_value)[
+                                : field.max_length
+                            ]  # Truncate the value if too long
+
+                    # Inside the loop where you handle ForeignKey fields
+                    if isinstance(field, ForeignKey):
+                        field_name = (
+                            field.name + "_id"
+                        )  # Get the name of the corresponding foreign key ID field
+                        field_value = defaults.get(field_name)
+                        if pd.isna(field_value) or field_value == "":
+                            defaults[field.name] = (
+                                None  # Set the ForeignKey field to None if its corresponding ID field is empty
+                            )
+                        else:
+                            try:
+                                # Fetch the User instance based on the provided ID
+                                user_instance = User.objects.get(id=field_value)
+                                defaults[field.name] = user_instance
+                            except User.DoesNotExist:
+                                # Handle the case where the user with the provided ID doesn't exist
+                                # You can log a warning or handle this according to your application's logic
+                                self.stdout.write(
+                                    self.style.WARNING(
+                                        f"User with ID {field_value} does not exist."
+                                    )
+                                )
+                                defaults[field.name] = (
+                                    None  # Assign None if user doesn't exist
+                                )
+                        # Remove the corresponding ID field from defaults since it's handled
+                        defaults.pop(field_name, None)
+
+                    # Inside the loop where you handle DateField fields
+                    # Inside the loop where you handle DateField fields
+                    if isinstance(field, DateField):
+                        field_value = defaults.get(field.name)
+                        if pd.isna(field_value) or field_value == "":
+                            defaults[field.name] = None  # Set the DateField to None if it's empty
+                        else:
+                            try:
+                                # Remove timezone information from the date string
+                                # field_value_without_timezone = field_value.split('+')[0]
+                                # # Attempt to convert the field value to a date
+                                # field_date = pd.to_datetime(field_value_without_timezone)
+                                defaults[field.name] = field_value  # Extract the date part
+                            except ValueError:
+                                # Handle the case where the value cannot be converted to a date
+                                # You can log a warning or handle this according to your application's logic
+                                self.stdout.write(self.style.WARNING(f"Invalid date format for field {field.name}: {field_value}"))
+                                defaults[field.name] = None  # Assign None if the value is invalid
+
+                # Create or update user object
+                user_obj, created = User.objects.update_or_create(
+                    id=user_id, defaults=defaults
                 )
                 if created:
                     created_count += 1
-                    self.stdout.write(
-                        self.style.SUCCESS(f"Created new user: {user.username}")
-                    )
                 else:
                     updated_count += 1
-                    self.stdout.write(
-                        self.style.SUCCESS(f"Updated user: {user.username}")
-                    )
 
-                # Close files after they are no longer needed
-                if image_file:
-                    image_file.close()
-                if background_file:
-                    background_file.close()
-
-            except IntegrityError as e:
+            except User.DoesNotExist:
+                self.stdout.write(
+                    self.style.ERROR(f"User with ID {user_id} not found.")
+                )
+            except Exception as e:
                 self.stdout.write(
                     self.style.WARNING(
-                        f'Failed to import user: {row["username"]}. Error: {str(e)}'
+                        f"Error occurred while importing or updating user: {e}"
                     )
                 )
-                ignored_count += 1
-                continue
 
-        self.stdout.write(self.style.SUCCESS("Import completed. Summary:"))
+        # Print summary
+        self.stdout.write(self.style.SUCCESS(f"Import completed. Summary:"))
         self.stdout.write(self.style.SUCCESS(f"Created: {created_count} users"))
-        self.stdout.write(self.style.SUCCESS(f"Updated: {updated_count} users"))
         self.stdout.write(
-            self.style.SUCCESS(f"Ignored: {ignored_count} entries due to errors")
+            self.style.SUCCESS(f"Updated: {updated_count} users due to updates")
         )
